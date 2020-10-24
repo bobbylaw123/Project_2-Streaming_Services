9a6a4ca3358d8810c9a1426097b44e3ae51f1020
bobbylaw123 <68624176+bobbylaw123@users.noreply.github.com>
2020-10-24 03:13:40 -0400

    Full Copy with Individual Work and Merged Work
    Also included is the sql query to run in postgres.

---
+++ query.sql
@@ -0,0 +1,9 @@
+CREATE TABLE streaming_services AS
+SELECT netflix.*
+	   , CASE WHEN "Hulu" IS NULL THEN 'Nope' ELSE "Hulu" END "Hulu"
+	   , CASE WHEN "Prime" IS NULL THEN 'Nope' ELSE "Prime" END "Prime"
+FROM netflix
+LEFT JOIN hulu ON netflix."Title of Show" = hulu."Title of Show"
+LEFT JOIN prime ON netflix."Title of Show" = prime."Title of Show";
+SELECT *
+FROM streaming_services;
