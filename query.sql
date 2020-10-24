02ec9be81fc25d961df383fffabac4c09c57afdf
bobbylaw123 <68624176+bobbylaw123@users.noreply.github.com>
2020-10-24 11:21:39 -0400

    Final Commit with Descriptions and Slight Modifications

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
-CREATE TABLE streaming_services AS
+CREATE TABLE netflix_vs_others AS
 SELECT netflix.*
 	   , CASE WHEN "Hulu" IS NULL THEN 'Nope' ELSE "Hulu" END "Hulu"
 	   , CASE WHEN "Prime" IS NULL THEN 'Nope' ELSE "Prime" END "Prime"
@@ -6,4 +6,4 @@ FROM netflix
 LEFT JOIN hulu ON netflix."Title of Show" = hulu."Title of Show"
 LEFT JOIN prime ON netflix."Title of Show" = prime."Title of Show";
 SELECT *
-FROM streaming_services;
+FROM netflix_vs_others;
